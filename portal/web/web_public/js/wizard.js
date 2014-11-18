96e3eb25f74478fd406b1904d7185c7f315d10f6
alcachi <alcachi@gmail.com>
2014-11-18 01:44:36 +0100

    Step 1: SSH key

--- portal/web/web_public/js/wizard.js
+++ portal/web/web_public/js/wizard.js
@@ -1,332 +1,257 @@
-function closeD(){
-  close();
-}
-
-function clear_key(){
-  document.getElementById("input_key").value="";
-}
-
-function request_add_key(){
-  var key = document.getElementById("input_key").value;
-
-  if(isValidKey(key)){
-    // TODO: Pass it from login screen instead of loading credentials.js
-    var json = {
-      username : user,
-      key : key
-    }
-    console.log(json);
-   
-    var request = $.ajax({
-      type: "POST",
-      url: "php/add_sshkey.php",
-      data: JSON.stringify(json),
-      dataType: "json",
-      success: function(data){
-        var print = "<b>ERROR</b> adding key '<u>"+
-          document.getElementById("input_key").value
-          +"</u>' for user "+user;
-        
-        if(data.message == "OK"){
-          var keyStr = document.getElementById("input_key").value;
-          keyStr = keyStr.substring(0,10) + '...' + keyStr.substring(keyStr.length-10);
-          print = "KEY '<u>"+ keyStr
-            +"</u>' <b>SUCCESSFULLY</b> added for user "+user;
+var KeyService = {
+    //TODO: FIXME: Rewrite get function
+    get: function(user) {
+        var json = {
+            user: user
         }
-        
-        document.getElementById("addKeyResponseDiv").innerHTML = print;
-        
-        request_sshkeys();
-      }
-    });
-   
-    var loader = $('<!-- 0 -->
-      <div class="loader" title="0">
-      <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-  <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
-    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
-    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
-  <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
-    C22.32,8.481,24.301,9.057,26.013,10.047z">
-    <animateTransform attributeType="xml"
-      attributeName="transform"
-      type="rotate"
-      from="0 20 20"
-      to="360 20 20"
-      dur="0.5s"
-      repeatCount="indefinite"/>
-    </path>
-  </svg>
-</div>');
-
-    loader.appendTo($(#addKeyResponseDiv));
-
-    alert('You have added a key.\n'
-        +'Depending on the server load, database performance, number of registered keys and'
-        +' several other factors this operation can take a little.'
-        +' The page will reload information relative to'
-        +' sshkeys automatically after this operation ends.');
-  } else {
-    alert('You\'ve typed a non valid key');
-  }
-}
-
-// ** KEY VALIDATION FUNCTS ** //
-function isValidKey(key){
-  if(isValidDSAKey(key))
-    return true;
-  else if(isValidRSAKey(key))
-    return true;
-  return false;
-}
-
-function isValidDSAKey(key){
-  keyHead = key.substring(0,7);
-  if(keyHead!='ssh-dss')
-    return false;
-  if(key.length<(586))
-    return false;
-  if(key.indexOf("'")>-1)
-    return false;
-  return true;
-}
-
-function isValidRSAKey(key){
-  keyHead = key.substring(0,7);
-  if(keyHead!='ssh-rsa')
-    return false;
-  if(key.length<(380))
-    return false;
-  if(key.indexOf("'")>-1)
-    return false;
-  return true;
-}
-
-function doDelKeys(){
-  document.getElementById("keysDiv").style.display="block";
-  document.getElementById("keysDiv").innerHTML="Loading SSHKeys . . .";
-  request_sshkeys();
-}
-
-function request_sshkeys(){
-  var json = {
-    user : user
-  }
-
-  var request = $.ajax({
-    type: "POST",
-    url: "php/get_sshkeys.php",
-    data: JSON.stringify(json),
-    dataType: "json",
-    success: function(data){
-      var jsonData = jQuery.parseJSON(data);
-      var print = "You have not any sshkey stored in our database.";
-      if(jsonData.keys.length > 0){
-        print = "";
-        for(var i = 0 ; i < jsonData.keys.length ; i++){
-          label = '<span class="spanResponseLabel">KEY '+i+' </span>';
-          print += label+'<div class="keyDiv">'+jsonData.keys[i]+"</div>"
-          +'<span class="spanStopCluster" onclick="deleteKey(\''+jsonData.keys[i]+'\')">DELETE</span><br/>'
-          +"<br/>";
+        var request = $.ajax({
+            type: 'POST',
+            url: 'php/get_sshkeys.php',
+            data: JSON.stringify(json),
+            dataType: 'json',
+            success: function (data) {
+                var jsonData = jQuery.parseJSON(data);
+                var print = 'You have not any sshkey stored in our database.';
+                if (jsonData.keys.length > 0) {
+                    print = '';
+                    for (var i = 0; i < jsonData.keys.length; i++) {
+                        label = '<span class="spanResponseLabel">KEY ' + i + ' </span>';
+                        print += label + '<div class="keyDiv">' + jsonData.keys[i] + '</div>'
+                        + '<span class="spanStopCluster" onclick="deleteKey(\'' + jsonData.keys[i] + '\')">DELETE</span><br/>'
+                        + '<br/>';
+                    }
+                }
+                $('addKeyResponseDiv').html(print);
+                //document.getElementById('keysDiv').innerHTML = print;
+            }
+        });
+    }
+    ,add: function() {
+        var key = document.getElementById('input_key').value;
+        if (this.validate(key)) {
+            // TODO: Pass it from login screen instead of loading credentials.js
+            var json = {
+                username: user,
+                key: key
+            }
+            console.log(json);
+            var request = $.ajax({
+                type: 'POST',
+                url: 'php/add_sshkey.php',
+                data: JSON.stringify(json),
+                dataType: 'json',
+                timeout: 5000,
+                success: function(data) {
+                    if (data.message == 'OK') {
+                        $('<div class="alert alert-success" role="alert">Key succesfully configured</div>').appendTo('#messages').fadeOut(3000);
+                        //TODO: Check if we need to list all keys or not
+                        //KeyService.get(user);
+                        var keyType = key.substring(0, 7);
+                        var re = /= (.+)$/;
+                        var match = re.exec(key);
+                        var keyLabel="N/A";
+                        if (match != null) {
+                            keyLabel = match[1];
+                        }
+                        $('#ssh_keys_table > tbody:last').prepend('<tr><td>'+keyType+'</td><td>'+keyLabel+'</td><td></td></tr>');
+                    } else {
+                        $('<div class="alert alert-danger alert-dismissible" role="alert">Unknown error adding key</div>').appendTo('#messages');
+                    }
+                },
+                error: function(xhrObj, textStatus, exception) {
+                    $('<div class="alert alert-danger alert-dismissible" role="alert">Error adding key</div>').appendTo('#messages');
+                }
+            });
+            $('<div class="alert alert-info" role="alert">Adding the new key to database</div>').appendTo('#messages').fadeOut(2000);
+        } else {
+            $('<div class="alert alert-danger alert-dismissible" role="alert">You\'ve typed a non valid key</div>').appendTo('#messages');
         }
-      }
-      document.getElementById("keysDiv").innerHTML=print;
     }
-  });
-
-}
-
-function deleteKey(key){
-  var json = {
-    username : user,
-    key : key
-  }
-  var request = $.ajax({
-    type: "POST",
-    url: "php/del_sshkey.php",
-    data: JSON.stringify(json),
-    dataType: "json",
-    success: function(data){
-      request_sshkeys();
-    }
-  });
-  alert('You have deleted a key.\nDepending on the server load, database performance, number of registered keys and'
-        +' several other factors this operation can take a little. The page will reload information relative to'
-        +' sshkeys automatically after this operation ends.');
-}
-
-
-var x=0 // variable declarad e iniciada como cero
-
-function operacion(){
-  if(x==0){
-    //solo entra aquí cuando x sea cero
-    document.getElementById("input_ip").value=document .getElementById("texto").value;
-    x++; // variable cambia a uno despues de copiar el texto.
-  } else {
-    //solo entra aquí cuando x sea uno
-    document.formulario.submit(); //aquí envias el form
-  }
-}
-
-function clear_ip(){
-  document.getElementById("input_ip").value="";
-}
-
-function request_add_ip(){
-  var ip = document.getElementById("input_ip").value;
-  if(isValidIp(ip)){
-    var json = {
-      uusername : user,
-      key : key
-    }
-    var request = $.ajax({
-      type: "POST",
-      url: "php/add_ip.php",
-      data: JSON.stringify(json),
-      dataType: "json",
-      success: function(data){
-        var print = "<b>ERROR</b> adding IP '<u>"+
-          document.getElementById("input_ip").value
-          +"</u>' for user "+user;
-        
-        if(data.message == "OK"){
-          print = "IP '<u>"+document.getElementById("input_ip").value
-            +"</u>' <b>SUCCESSFULLY</b> added for user "+user;
+    ,validate: function(key) {
+        keyHead = key.substring(0, 7);
+        // DSA
+        if (keyHead == 'ssh-dss') {
+          if (key.length < (586))
+              return false;
+          if (key.indexOf('\'') > - 1)
+              return false;
+          return true;
+        } else if (keyHead == 'ssh-rsa') {
+            // RSA
+            if (key.length < (380))
+                return false;
+            if (key.indexOf('\'') > - 1)
+                return false;
+            return true;
         }
-        
-        document.getElementById("addIPResponseDiv").innerHTML = print;
-        
-        request_ips();
-      }
-    });
-    alert('You have added an IP.\nDepending on the server load, database performance, number of registered ips and'
-        +' several other factors this operation can take a little. The page will reload information relative to'
-        +' ips automatically after this operation ends.');
-  } else {
-    alert('You\'ve typed a non valid IP');
-  }
-}
-
-// ** IP VALIDATION FUNCTS ** //
-function isValidIp(ip){
-  dotCount = 0;
-  barCount = 0;
-  
-  for(var i=0 ; i<ip.length ; i++){
-    if(!isValidIPChar(ip.charAt(i))){ // Es una caracter no valido
-      console.log("No valid char found: "+ip.charAt(i));
-      return false;
-    }else{ // Es un caracter valido
-      if(ip.charAt(i)=='.'){ // Es un punto
-        dotCount++;
-        if(dotCount>3)
-          return false;
-      }
-      
-      if(ip.charAt(i)=='/'){ // Es una barra
-        barCount++;
-        if(barCount>1)
-          return false;
-      }
+        return false;
     }
-  }
-  
-  if(dotCount<3) // No hay suficientes puntos como para que sea una IP completa
-    return false;
-  
-  splitted = ip.split(".");
-  for(var i = 0 ; i < splitted.length && i <= 3 ; i++){
-    var num = parseInt(splitted[i]);
-    if(num>255)
-      return false;
-  }
-  
-  return true;
-}
-
-function isValidIPChar(c){
-  validChars = ['1','2','3','4','5','6','7','8','9','0','.','/']; 
-  
-  for(var i = 0 ; i < validChars.length ; i++){
-    if(validChars[i]==c)
-      return true;
-  }
-  
-  return false;
-}
-
-function doDelIps(){
-  document.getElementById("ipsDiv").style.display="block";
-  document.getElementById("ipsDiv").innerHTML="Loading IPs . . .";
-  
-  request_ips();
-}
-
-function request_ips(){
-  var json = {
-    user : user
-  }
-  
-  var request = $.ajax({
-    type: "POST",
-    url: "php/get_ips.php",
-    data: JSON.stringify(json),
-    dataType: "json",
-    success: function(data){
-      var jsonData = jQuery.parseJSON(data);
-      var print = "You have not any IP stored in our database.";
-      
-      if(jsonData.ips.length > 0){
-        print = "";
-        for(var i = 0 ; i < jsonData.ips.length ; i++){
-          label = '<span class="spanResponseLabel">IP </span>';
-          print += label+jsonData.ips[i]
-            +'<span class="spanStopCluster" onclick="deleteIp(\''+jsonData.ips[i]+'\')">DELETE</span><br/>'
-            +"<br/>";
+    ,clear: function() {
+        document.getElementById('input_key').value = '';
+    }
+    ,delete: function(key){
+        var json = {
+            username: user,
+            key: key
         }
-      }
-      
-      document.getElementById("ipsDiv").innerHTML=print;
-      
-      
+        var request = $.ajax({
+            type: 'POST',
+            url: 'php/del_sshkey.php',
+            data: JSON.stringify(json),
+            dataType: 'json',
+            success: function (data) {
+                request_sshkeys();
+            }
+        });
+        alert('You have deleted a key.\nDepending on the server load, database performance, number of registered keys and'
+        + ' several other factors this operation can take a little. The page will reload information relative to'
+        + ' sshkeys automatically after this operation ends.');
     }
-  });
+};
+
+var IPService = {
+    clear: function() {
+        document.getElementById('input_ip').value = '';
+    }
+    ,add: function() {
+        var ip = document.getElementById('input_ip').value;
+        if (this.validate(ip)) {
+            var json = {
+                uusername: user,
+                key: key
+            }
+            var request = $.ajax({
+                type: 'POST',
+                url: 'php/add_ip.php',
+                data: JSON.stringify(json),
+                dataType: 'json',
+                success: function (data) {
+                    var print = '<b>ERROR</b> adding IP \'<u>' +
+                    document.getElementById('input_ip').value
+                    + '</u>\' for user ' + user;
+                    if (data.message == 'OK') {
+                        print = 'IP \'<u>' + document.getElementById('input_ip').value
+                        + '</u>\' <b>SUCCESSFULLY</b> added for user ' + user;
+                    }
+                    document.getElementById('addIPResponseDiv').innerHTML = print;
+                    IPService.get(user);
+                }
+            });
+            alert('You have added an IP.\nDepending on the server load, database performance, number of registered ips and'
+            + ' several other factors this operation can take a little. The page will reload information relative to'
+            + ' ips automatically after this operation ends.');
+        } else {
+            alert('You\'ve typed a non valid IP');
+        }
+    }
+    ,validate: function(ip) {
+        function isValidIPChar(c) {
+            validChars = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '/' ];
+            for (var i = 0; i < validChars.length; i++) {
+                if (validChars[i] == c)
+                    return true;
+            }
+            return false;
+        }
+        dotCount = 0;
+        barCount = 0;
+        for (var i = 0; i < ip.length; i++) {
+            if (!isValidIPChar(ip.charAt(i))) { // Es una caracter no valido
+                console.log('No valid char found: ' + ip.charAt(i));
+                return false;
+            } else { // Es un caracter valido
+                if (ip.charAt(i) == '.') { // Es un punto
+                    dotCount++;
+                    if (dotCount > 3)
+                    return false;
+                }
+                if (ip.charAt(i) == '/') { // Es una barra
+                    barCount++;
+                    if (barCount > 1)
+                    return false;
+                }
+            }
+        }
+        if (dotCount < 3) // No hay suficientes puntos como para que sea una IP completa
+        return false;
+        splitted = ip.split('.');
+        for (var i = 0; i < splitted.length && i <= 3; i++) {
+            var num = parseInt(splitted[i]);
+            if (num > 255)
+            return false;
+        }
+        return true;
+    }
+    ,get: function(user) {
+        var json = {
+            user: user
+        }
+        var request = $.ajax({
+            type: 'POST',
+            url: 'php/get_ips.php',
+            data: JSON.stringify(json),
+            dataType: 'json',
+            success: function (data) {
+                var jsonData = jQuery.parseJSON(data);
+                var print = 'You have not any IP stored in our database.';
+                if (jsonData.ips.length > 0) {
+                    print = '';
+                    for (var i = 0; i < jsonData.ips.length; i++) {
+                        label = '<span class="spanResponseLabel">IP </span>';
+                        print += label + jsonData.ips[i]
+                        + '<span class="spanStopCluster" onclick="deleteIp(\'' + jsonData.ips[i] + '\')">DELETE</span><br/>'
+                        + '<br/>';
+                    }
+                }
+                document.getElementById('ipsDiv').innerHTML = print;
+            }
+        });
+    }
+    ,delete: function(ip) {
+        var json = {
+            username: user,
+            ip: ip
+        }
+        var request = $.ajax({
+            type: 'POST',
+            url: 'php/del_ip.php',
+            data: JSON.stringify(json),
+            dataType: 'json',
+            success: function (data) {
+                request_ips();
+            }
+        });
+        alert('You have deleted an IP.\nDepending on the server load, database performance, number of registered IPs and'
+        + ' several other factors this operation can take a little. The page will reload information relative to'
+        + ' IPs automatically after this operation ends.');
+    }
+};
+
+// TODO: Para que sirve esto?
+var x = 0 // variable declarad e iniciada como cero
+function operacion() {
+    if (x == 0) {
+        //solo entra aquí cuando x sea cero
+        document.getElementById('input_ip').value = document.getElementById('texto').value;
+        x++; // variable cambia a uno despues de copiar el texto.
+    } else {
+        //solo entra aquí cuando x sea uno
+        document.formulario.submit(); //aquí envias el form
+    }
 }
 
-function deleteIp(ip){
-  var json = {
-    username : user,
-    ip : ip
-  }
-  
-  var request = $.ajax({
-    type: "POST",
-    url: "php/del_ip.php",
-    data: JSON.stringify(json),
-    dataType: "json",
-    success: function(data){
-      request_ips();
-    }
-  });
-  
-  alert('You have deleted an IP.\nDepending on the server load, database performance, number of registered IPs and'
-        +' several other factors this operation can take a little. The page will reload information relative to'
-        +' IPs automatically after this operation ends.');
-  
+function closeD() {
+    close();
 }
 
 // Setup: https://github.com/mstratman/jQuery-Smart-Wizard
-$(document).ready(
-    function(){
-      // Smart Wizard   
-      $('#wizard').smartWizard({
+$(document).ready(function () {
+    // Smart Wizard   
+    $('#wizard').smartWizard({
         onFinish: onFinishCallback
-      });
-
-      function onFinishCallback(){
-        $('#wizard').smartWizard('showMessage','Finish Clicked');
-      }
+    });
+    function onFinishCallback() {
+        $('#wizard').smartWizard('showMessage', 'Finish Clicked');
     }
+}
 );
